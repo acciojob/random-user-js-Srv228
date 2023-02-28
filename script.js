e008c858db0fe629d16f2fd9e021b8e78d6d0805
Sourav Dey <102379824+Srv228@users.noreply.github.com>
2023-02-28 09:59:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
 //your code here
+
+let photo = document.getElementById("photo")
+let fullname = document.getElementById("name");
+
+let getAnotherUser = document.getElementById("getUser")
+let btn1 = document.querySelector('[data-attr="age"]')
+let btn2 = document.querySelector('[data-attr="email"]')
+let btn3 = document.querySelector('[data-attr="phone"]')
+
+getAnotherUser.addEventListener("click" ,GetUser);
+window.load = GetUser();
+function GetUser(){
+    let details = document.getElementById("info")
+    details.innerText = " ";
+
+fetch("https://randomuser.me/api/")
+.then((response)=>response.json())
+.then((data)=>{
+    console.log("data" ,data);
+    let details = data.results;
+    console.log(details);
+    details.map((results)=>{
+        let pictures = results.picture.large;
+        console.log(pictures);
+        photo.src = pictures;
+        
+        
+        fullname.innerText= `${results.name.first} ${results.name.last}`;
+        
+        let details = document.getElementById("info")
+        btn1.addEventListener("click" ,()=>{
+            
+            details.innerText =results.dob.age;
+            
+            
+        })
+        btn2.addEventListener("click" ,()=>{
+            details.innerHTML = results.email;
+            
+        })
+        btn3.addEventListener("click" ,()=>{
+            details.innerHTML = results.phone;
+            
+        })
+        
+       
+
+    })
+})
+}
